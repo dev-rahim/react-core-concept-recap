9b47da89caf4a3f60921b0dc17c784d71a9747c0
dev-rahim <rahimuddin5915@gmail.com>
2022-10-12 21:42:45 +0600

    react 6 core concept recap done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const articleHeader = {
+    color: 'salmon',
+    textAlign: 'center',
+    marginTop: '0px',
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Mobile></Mobile>
+      <h2>Blogs</h2>
+      <article className='blog'>
+        <h2 style={articleHeader}>Lorem, ipsum dolor.</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum debitis tenetur nobis facere ipsa sapiente error ipsam eligendi eos aperiam maxime quia, repellendus tempora animi! Facilis ab assumenda voluptas repellendus.</p>
+      </article>
+
+      <Blog author='Rahim Uddin' heading="Rahim's Blog"></Blog>
+      <Blog author='Kabir Sing' heading='Kabirs Blog'></Blog>
+      <Blog author='Sonia' heading="Sonia's Blog"></Blog>
     </div>
   );
+}
+
+function Mobile() {
+  const [battery, setBattery] = useState(100);
+  const DecreaseBattery = () => {
+    if (battery === 0) {
+      return;
+    }
+    setBattery(battery - 10);
+  }
+  return (
+    <div>
+      <h3>{battery}%</h3>
+      <button onClick={DecreaseBattery}>Battery Down</button>
+    </div>
+  )
+}
+
+function Blog(props) {
+  return (
+    <div style={{ textAlign: 'center' }} className='blog'>
+      <h2>Heading: {props.heading}</h2>
+      <h5>Author Name: {props.author}</h5>
+    </div>
+  )
 }
 
 export default App;
